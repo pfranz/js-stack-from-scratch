49ee07b7e2704803d32ac6a78f2d800daeae8c5b
Paul Franz <paul@paulfranz.com>
2017-03-28 06:18:58 +0000

    ch02 - created es6 class

---
+++ src/dog.js
@@ -0,0 +1,10 @@
+class Dog {
+    constructor(name) {
+        this.name = name
+    }
+    bark() {
+        return `Wah wah, I am ${this.name}`
+    }
+}
+
+module.exports = Dog
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
-const str = 'ES6';
-console.log(`Hello ${str}`);
+const Dog = require('./dog')
+const toby = new Dog('Toby')
+console.log(toby.bark());
