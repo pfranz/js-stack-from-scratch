e885ed1507ed910dca32c24a3d13719d610c70fb
pfranz <paul@paulfranz.com>
2017-03-31 14:45:26 -0700

    Unmodified name

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Hello App Modified'
+export const APP_NAME = 'Hello App'
 
 export const WDS_PORT = 7000
 
